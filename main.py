493cdf6d057e3408ab80a08a3cacd2d5bcc04ecd
gmdz <35648177+gio328@users.noreply.github.com>
2025-02-24 20:42:39 -0800

    clean main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import gradio as gr
-from tickets import tools, get_ticket_price, handle_tool_call
+from tickets import tools, handle_tool_call
 from config import openai_instance as openai
 
 
 model_use = 'gpt-4o-mini'
 system_prompt = "You are a helpful assistant."
-history = []    
 
 def message_gpt(message, history):
     messages = [{'role': 'system', 'content': system_prompt}] + history + [{'role': 'user', 'content': message}]
@@ -23,7 +22,6 @@ def message_gpt(message, history):
     if response.choices[0].finish_reason=="tool_calls":
         print("Tool calls detected:")
         message = response.choices[0].message
-        print('Message:', message)
         response = handle_tool_call(message)
         messages.append(message)
         messages.append(response)
@@ -40,19 +38,3 @@ interface = gr.ChatInterface(
 )
 
 interface.launch()
-
-##########################################################################################
-# ChatCompletionChunk
-# ChatCompletionMessage(
-#     content=None, 
-#     refusal=None, 
-#     role='assistant', 
-#     audio=None, 
-#     function_call=None, 
-#     tool_calls=[ChatCompletionMessageToolCall(
-#                     id='call_XPPnEpqbuH9591fHC3NpXOcl', 
-#                     function=Function(arguments='{"destination_city":"Philippines"}', 
-#                     name='get_ticket_price'), 
-#                     type='function')
-#                     ]
-# )
